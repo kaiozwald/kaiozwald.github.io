ed5b8456eab84a348ae4c916148c4e5809d84c6f
Abdallah Abuhassan <abdallah.mohamed1700091@ci.menofia.edu.eg>
2024-07-03 06:22:24 +0300

    commit porti

---
+++ precache-manifest.4429225a6a8d46fef3c891932f3dd8f1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f100ff79c83d2997bef5dc28121080a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34adb70c88ae72889d58",
+    "url": "/static/css/main.5b4f3cf3.chunk.css"
+  },
+  {
+    "revision": "cf615b0c0a7345facfbe",
+    "url": "/static/js/2.ce06e740.chunk.js"
+  },
+  {
+    "revision": "34adb70c88ae72889d58",
+    "url": "/static/js/main.a6c49a6e.chunk.js"
+  },
+  {
+    "revision": "9c4fb0ed10011dc75bae",
+    "url": "/static/js/runtime-main.661f12f8.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "c1f9993b4fce219491d533f3f358fd86",
+    "url": "/static/media/developerActivity.c1f9993b.svg"
+  },
+  {
+    "revision": "17d89cc2ec3e6887c10a08081125beae",
+    "url": "/static/media/fb.17d89cc2.jpg"
+  },
+  {
+    "revision": "180d53e4c78f75921ba89b1f9f3765c8",
+    "url": "/static/media/flutter.180d53e4.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "b61c4fbeecd73fc412296f8f182c94bd",
+    "url": "/static/media/manOnTable.b61c4fbe.svg"
+  },
+  {
+    "revision": "8b02dd642c6fb0ef96695d31399d42b7",
+    "url": "/static/media/ngtalk.8b02dd64.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "632fb58004e557f793eb9555901d7f44",
+    "url": "/static/media/susi.632fb580.jpg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/static/media/talksCardBack.de3da806.svg"
+  },
+  {
+    "revision": "b8dbbf13c851f80cf3922b8aa14d74a0",
+    "url": "/static/media/ted.b8dbbf13.jpg"
+  },
+  {
+    "revision": "419ec8592294f75a37b10a5978303a76",
+    "url": "/static/media/tv.419ec859.jpg"
+  },
+  {
+    "revision": "0fe7922a5a24fca23109e6218878fdbe",
+    "url": "/static/media/ventire.0fe7922a.png"
+  }
+]);
